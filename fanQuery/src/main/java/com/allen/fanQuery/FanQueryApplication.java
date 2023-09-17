60c3500509da9faf19e342a3493076d4f973d13f
david <dy2836966@gmail.com>
2023-09-17 16:45:51 +0400

    11

---
+++ fanQuery/src/main/java/com/allen/fanQuery/FanQueryApplication.java
@@ -0,0 +1,13 @@
+package com.allen.fanQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanQueryApplication.class, args);
+	}
+
+}
---
+++ fanQuery/src/test/java/com/allen/fanQuery/FanQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.fanQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
